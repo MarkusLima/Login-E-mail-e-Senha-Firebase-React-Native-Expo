c3c2c539bdf97991618e096b032cf4e356f71c96
MarkusLima <47782867+MarkusLima@users.noreply.github.com>
2020-06-16 12:02:25 -0300

    16/06/2020
    Co-Authored-By: markuslima <markuslima@users.noreply.github.com>

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB0-lP6xZSygAKzHXAm5NZe99lSxQ0SsDQ",
+    authDomain: "loginfirebase-287d3.firebaseapp.com",
+    databaseURL: "https://loginfirebase-287d3.firebaseio.com",
+    projectId: "loginfirebase-287d3",
+    storageBucket: "loginfirebase-287d3.appspot.com",
+    messagingSenderId: "702075328948",
+    appId: "1:702075328948:web:0263eb06f2d16762023ae1",
+    measurementId: "G-C7WBF9S86P"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ export default firebase; 
